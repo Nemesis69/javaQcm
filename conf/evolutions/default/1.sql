dd91099c9939b56f0c22575c3f8e14b8112b3e5b
Nemesis69 <nemesis29@free.fr>
2013-06-26 09:54:09 +0200

    refactoring

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -11,11 +11,19 @@ create table choice (
   constraint pk_choice primary key (id))
 ;
 
+create table domain (
+  id                        bigint auto_increment not null,
+  libelle                   varchar(255),
+  constraint uq_domain_libelle unique (libelle),
+  constraint pk_domain primary key (id))
+;
+
 create table qcm (
   id                        bigint auto_increment not null,
   name                      varchar(255),
-  category                  varchar(255),
+  category                  integer,
   max_score                 decimal(38),
+  constraint ck_qcm_category check (category in (0,1)),
   constraint uq_qcm_name unique (name),
   constraint pk_qcm primary key (id))
 ;
@@ -23,8 +31,9 @@ create table qcm (
 create table question (
   id                        bigint auto_increment not null,
   text                      varchar(255),
-  domain_id                 bigint,
+  domainId                  bigint,
   qcmId                     bigint,
+  domain_id_value           varchar(255),
   constraint pk_question primary key (id))
 ;
 
@@ -48,14 +57,16 @@ create table user (
 
 alter table choice add constraint fk_choice_questionRef_1 foreign key (questionId) references question (id) on delete restrict on update restrict;
 create index ix_choice_questionRef_1 on choice (questionId);
-alter table question add constraint fk_question_qcm_2 foreign key (qcmId) references qcm (id) on delete restrict on update restrict;
-create index ix_question_qcm_2 on question (qcmId);
-alter table response add constraint fk_response_choice_3 foreign key (choiceId) references choice (id) on delete restrict on update restrict;
-create index ix_response_choice_3 on response (choiceId);
-alter table response add constraint fk_response_question_4 foreign key (questionId) references question (id) on delete restrict on update restrict;
-create index ix_response_question_4 on response (questionId);
-alter table response add constraint fk_response_user_5 foreign key (userId) references user (id) on delete restrict on update restrict;
-create index ix_response_user_5 on response (userId);
+alter table question add constraint fk_question_domain_2 foreign key (domainId) references domain (id) on delete restrict on update restrict;
+create index ix_question_domain_2 on question (domainId);
+alter table question add constraint fk_question_qcm_3 foreign key (qcmId) references qcm (id) on delete restrict on update restrict;
+create index ix_question_qcm_3 on question (qcmId);
+alter table response add constraint fk_response_choice_4 foreign key (choiceId) references choice (id) on delete restrict on update restrict;
+create index ix_response_choice_4 on response (choiceId);
+alter table response add constraint fk_response_question_5 foreign key (questionId) references question (id) on delete restrict on update restrict;
+create index ix_response_question_5 on response (questionId);
+alter table response add constraint fk_response_user_6 foreign key (userId) references user (id) on delete restrict on update restrict;
+create index ix_response_user_6 on response (userId);
 
 
 
@@ -64,6 +75,8 @@ create index ix_response_user_5 on response (userId);
 SET FOREIGN_KEY_CHECKS=0;
 
 drop table choice;
+
+drop table domain;
 
 drop table qcm;
 
